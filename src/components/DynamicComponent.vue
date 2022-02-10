6886e01f04764b9133d6f8178c31584095c597b8
negin-khademian <neginkhademian@gmail.com>
2022-02-10 16:53:51 +0330

    feat: add base of dynamic component

---
+++ src/components/DynamicComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  render(createElement) {
+    return createElement(this.dynamicComponent, {});
+  },
+  computed: {
+    dynamicComponent() {
+      return {
+        inheritAttrs: true,
+        template: `<div>test</div>`,
+      };
+    },
+  },
+};
+</script>
